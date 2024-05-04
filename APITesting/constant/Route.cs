7fc9097f11ceb50e5d4fbf3e6ee511b11af61a9e
lyngo-ntkl <lyngo.ntkl@gmail.com>
2024-05-04 13:04:21 +0700

    refactor: add route for testing

---
+++ APITesting/constant/Route.cs
@@ -0,0 +1,8 @@
+using Microsoft.Extensions.Hosting;
+
+namespace Constant {
+    public class Route {
+        public const string Host = "https://localhost:7258";
+        public const string BaseUrl = "/v1/api";
+    }
+}
